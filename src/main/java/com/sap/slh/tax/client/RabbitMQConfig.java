2ba30984bc774208304e11b787590e66aab1b57d
I347159 <I347159@INLN56593992A.apj.global.corp.sap>
2020-03-31 19:52:16 +0530

    commiting changes for rabbitmq client

--- src/main/java/com/sap/slh/tax/client/RabbitMQConfig.java
+++ src/main/java/com/sap/slh/tax/client/RabbitMQConfig.java
@@ -1,7 +1,10 @@
 package com.sap.slh.tax.client;
 
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,13 @@ public class RabbitMQConfig {
 	@Bean
 	public ConnectionFactory connectionFactory() {
 	return new CachingConnectionFactory();
+	}
+	
+	@Bean
+	public AsyncRabbitTemplate asyncRabbitTemplate(RabbitTemplate rabbitTemplate)
+	{
+		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+		return new AsyncRabbitTemplate(rabbitTemplate);
 	}
 
 }
